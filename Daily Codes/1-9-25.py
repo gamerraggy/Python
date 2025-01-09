20f37f70b9609b0e8a1d5ad748bac3b764db7638
gamerraggy <javib0414@gmail.com>
2025-01-09 09:27:08 -0700

    Create 1-9-25.py

---
+++ Daily Codes/1-9-25.py
@@ -0,0 +1,26 @@
+#javi
+#01/09/25
+#time: 8:51 AM - 9:17 AM (I did help raidin and mykail)
+def add_numbers(a, b): #starter function definition
+    return a + b
+
+def circleArea(a): #function definition
+    return a * a * 3.14
+
+def FtoC(a): #function definition
+    return (a - 32) * 5/9
+
+def AverageOfThree(a, b, c): #function definition
+    return (a + b + c) / 3
+
+result = add_numbers(5,3) #function declaration
+print("The sum of the numbers is:", result) #function result
+
+result = circleArea(5) #function declaration
+print("The area of circle is:", result) #function result
+
+result = FtoC(95) #function declaration
+print("The degrees in celcius:", result) #function result
+
+result = AverageOfThree(5, 7, 3) #function declaration
+print("The average is:", result) #function result
