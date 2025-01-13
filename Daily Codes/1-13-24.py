a3e8b784277dc8737a997e1b8f47dbcdcfe846f3
gamerraggy <javib0414@gmail.com>
2025-01-13 09:08:19 -0700

    Create 1-13-24.py

---
+++ Daily Codes/1-13-24.py
@@ -0,0 +1,22 @@
+def CirlceCirfumference(a):
+    return 2 * 3.14 * a
+    
+result = CirlceCirfumference(5) #function declaration
+print("The cirfumference of circle is:", result) #function result
+
+
+def isEven(num):
+    if num % 2 == 0:
+        return True
+    else:
+        return False
+    
+
+result = isEven(5)
+print (result)
+
+def frog(num2):
+    for frog in range(num2):
+        print("ribbit")
+        
+result = frog(5)
