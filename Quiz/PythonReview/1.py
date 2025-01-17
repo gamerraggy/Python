31783c3681475f2d14a179702675cde04c09126c
gamerraggy <javib0414@gmail.com>
2025-01-17 09:19:33 -0700

    Create 1.py

---
+++ Quiz/PythonReview/1.py
@@ -0,0 +1,8 @@
+x = int(input("Choose a number"))
+y = int(input("What is your second number"))
+if x > y:
+    print("FIRST ONE BIGGER")
+elif y > x:
+    print("SECOND ONE BIGGER")
+else:
+    print("Equal.")
