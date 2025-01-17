678c45db424daf599625196ec6653458f6eed212
gamerraggy <javib0414@gmail.com>
2025-01-17 09:20:14 -0700

    Delete Quiz/PythonReview directory

--- Quiz/PythonReview/1.py
+++
@@ -1,8 +0,0 @@
-x = int(input("Choose a number"))
-y = int(input("What is your second number"))
-if x > y:
-    print("FIRST ONE BIGGER")
-elif y > x:
-    print("SECOND ONE BIGGER")
-else:
-    print("Equal.")
