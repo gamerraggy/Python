b7876ee875fffec7e3c3145fadecbb8a1a9384ab
gamerraggy <javib0414@gmail.com>
2025-01-22 09:31:14 -0700

    Create Problem2.py

---
+++ Daily Codes/1-21-25/Problem2.py
@@ -0,0 +1,8 @@
+#convert kilometers to miles; write a function that converts distance in kilometers to miles and returns that value
+#call the function in a print statement
+#formula: miles = kilometers*.621371
+
+def kilotometers(k):
+    return k * .621371
+
+print(kilotometers(100)) #call
