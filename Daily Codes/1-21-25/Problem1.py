03e684cee4e5bcb8efdd2b3af65ff2610c7f258e
gamerraggy <javib0414@gmail.com>
2025-01-22 09:30:37 -0700

    Create Problem1.py

---
+++ Daily Codes/1-21-25/Problem1.py
@@ -0,0 +1,14 @@
+#create a function that calculates and returns the volume of a cylinden given its height and raidus
+#call the function in a print statement
+#formula = 3.14*radius*radius*height
+
+def cylinderarea(radius, height):
+    return 3.14*radius*radius*height
+
+print(cylinderarea(10,5))
+
+
+
+#1) look up and write out formula
+#2) ty[e "def" then the name, the the paremeters
+#3) type the formula after the return statement
